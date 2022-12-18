dc12da9bac42e02c2d482e6f428724842228cf66
MaxSvjatoha <maxsvjatoha@gmail.com>
2022-12-18 20:12:27 +0100

    generator initial version + gitignore

--- testing_ground.py
+++ testing_ground.py
@@ -5,4 +5,4 @@ from functions.tile_colormap_generator import *
 if __name__ == '__main__':
     
     for i in range(10):
-        generate_tile(size=(64, 64), colors=colormaps["dirt"]["colors"], probabilities=colormaps["dirt"]["probabilities"], show_tile=False, save=True, save_name="dirt_test_"+str(i)+".png")
+        generate_tile(size=(64, 64), colors=colormaps["grass"]["colors"], probabilities=colormaps["grass"]["probabilities"], show_tile=False, save=True, save_name="dirt_test_"+str(i)+".png")
